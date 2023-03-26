8da9f7f718694131af8e77239535556c173c935a
Rob B <rob@rjrobbie.dev>
2023-03-26 12:56:01 +0100

    ForescastSummary test suite passes

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 describe("ForecastSummary", () => {
+    const validProps = {
+        date: 1111111,
+        description: "Stub description",
+        icon: "stubIcon",
+        temperature: {
+            min: 12,
+            max: 22
+        }
+    }
+
+
     it("renders", () => {})
 });
