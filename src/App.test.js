60088fab3286756ad0701235f851cff70e959e04
Rob B <rob@rjrobbie.dev>
2023-03-26 12:45:31 +0100

    add ForecastSummary component and test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders weather app title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/weather app/i);
-  expect(linkElement).toBeInTheDocument();
-});
